17b8b111c8e4e3967afa4d0f5b6f35fb444c8fa6
Deeya <guptadeeya12@gmail.com>
2024-04-19 11:15:42 +0530

    Complete delete user listing functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -145,6 +145,27 @@ export default function Profile() {
           setShowListingError(true)
         }
       }
+
+      const handleListingDelete = async(listingId) =>{
+        try{
+          const res = await fetch(`/api/listing/delete/${listingId}`,{
+            method: 'DELETE'
+          })
+
+          const data = res.json()
+
+          if(data.success === false){
+            console.log(data.message)
+            return
+          }
+
+          setUserListing((prev) => prev.filter((listing) => listing._id !== listingId)
+          )
+        }
+        catch(error){
+          console.log(error.message)
+        }
+      }
   
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -206,7 +227,7 @@ export default function Profile() {
 
         {userListing && userListing.length > 0 && 
 
-        <div className="flex flexx gap-4">
+        <div className="flex flex-col gap-4">
           <h1 className="text-center mt-7 text-2xl font-semibold">Your listings</h1>
         {userListing.map((listing) =>(
           // items-center align the items vertically
@@ -220,7 +241,7 @@ export default function Profile() {
           </Link>
 
           <div className="flex flex-col items-center">
-            <button className="text-red-700">Delete</button>
+            <button onClick={() => handleListingDelete(listing._id)} className="text-red-700">Delete</button>
             <button className="text-green-700">Edit</button>
           </div>
         </div>
@@ -229,4 +250,4 @@ export default function Profile() {
         }
     </div>
   )
-}
+} 
